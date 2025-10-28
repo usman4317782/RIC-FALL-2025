4af33ee40babe9581e1394d640cac59b8f056a8e
usman4317782 <usman4317782@gmail.com>
2025-10-28 10:54:08 +0500

    updated

---
+++ PF/code/28-10-2025/home.cpp
@@ -0,0 +1,16 @@
+#include<iostream> //header file
+using namespace std;
+int main()
+{
+
+	int		age = 12;  //4 bytes
+	float	salary = 45.568;   // 4 bytes
+	double  distance = 1235.548;   // 8 bytes
+	char	grade = '1';  // 1 byte 0 1 T F A-Z
+	
+	cout << "User Age: " << age;
+			
+//	cout << "first program"; // static type
+	
+	return 0;
+}
