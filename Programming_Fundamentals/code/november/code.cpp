057705ac743f67dedf0a162bddab81de82d5b2f7
usman4317782 <usman4317782@gmail.com>
2025-11-03 12:52:54 +0500

    basics

---
+++ Programming_Fundamentals/code/november/code.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int     age; //458459784987
+    float   salary; 
+    double  distance;
+    char    grade;
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "Enter your salary: ";
+    cin >> salary;
+    cout << "Enter Distance Total Traveled: ";
+    cin >> distance;
+    cin >> grade;
+}
+
