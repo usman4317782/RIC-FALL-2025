d91ee45b4ac42ffbddf3bfb06b057075cc2ea61f
usman4317782 <usman4317782@gmail.com>
2025-11-07 10:15:17 +0500

    code updated

---
+++ AI ITM/3_code_Lab/07-11-2025 (Python Numpy)/lab_numpy.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+a = np.zeros((3,3))
+b = np.ones((2,4))
+c = np.full((2,2), 7)
+d = np.eye(3)  # Identity matrix
+e = np.arange(0, 10, 2)  # start, stop, step
+f = np.linspace(0, 1, 5)  # equally spaced
+
+
+print (e)
