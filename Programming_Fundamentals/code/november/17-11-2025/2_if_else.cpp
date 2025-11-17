0c549c96e3e173d29c5aae0496c6893026713113
usman4317782 <usman4317782@gmail.com>
2025-11-17 11:58:22 +0500

    update

---
+++ Programming_Fundamentals/code/november/17-11-2025/2_if_else.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	int a = 2, b = 4;
+	if(1==1 && 2==3)
+	{
+		cout << "if block executed";
+	}
+	else
+	{
+		cout << "else block executed";		
+	}
+	
+	return 0;
+}
---
+++ Programming_Fundamentals/code/november/17-11-2025/coamprisons.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	int a = 2, b = 4;
+	if(1==1 && 2==3)
+	{
+		cout << "if block executed";
+	}
+	else
+	{
+		cout << "else block executed";		
+	}
+}
+//	elseif
+//	cout << (1==1 && 2==3);
+//	cout << (1==1 || 2==3);
+	
+	return 0;
+}
