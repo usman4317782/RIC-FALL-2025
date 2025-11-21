2c0e46b661d4e2d6df7ed2bd39ac5d3d62211dc2
usman4317782 <usman4317782@gmail.com>
2025-11-21 08:50:44 +0500

    update

---
+++ Programming_Fundamentals/lab/18-11-2025/logical_ops.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	
+	
+//	&& || !
+
+//	And &&
+//	cout << (1==2 || 3==2)	;
+//	cout << "hello";
+	
+	int a = 0;
+	if(a != 0 || a > 0)
+	{
+		cout << "hello";		
+	}
+	else
+	{
+		cout << "nice try";
+	}
+
+	
+//	OR ||
+	
+//	Not !
+	
+	return 0;
+}
